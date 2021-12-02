33b9985589648f90ef367573a22140a36d79dc01
nicruo <nicruo@gmail.com>
2021-12-02 21:45:37 +0000

    Added more scenes;

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,21 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject squarePointer;
 
-    private Vector3 initialPosition;
+    private TextMeshProUGUI clockText;
+    public long secondsPassed = 0;
+
+
+    private static GameManager _instance;
+
+    public static GameManager Instance { get { return _instance; } }
+
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+
 
     void Start()
     {
-        initialPosition = squarePointer.transform.position;
+        StartCoroutine(Clock());
+    }
+
+
+    IEnumerator Clock()
+    {
+        while (true)
+        {
+            clockText = GameObject.Find("ClockText").GetComponent<TextMeshProUGUI>();
+            TimeSpan time = TimeSpan.FromSeconds(secondsPassed);
+            string str = time.ToString(@"mm\:ss");
+
+            clockText.text = str;
+            secondsPassed++;
+
+            yield return new WaitForSeconds(1);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        squarePointer.transform.position = initialPosition + Input.mousePosition / 100;
+        
     }
+
+    
 }
